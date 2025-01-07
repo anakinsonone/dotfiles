6a1704ff772b4b485587d951687a6412b3815dac
anakinsonone <akkhi.sonone@gmail.com>
2025-01-07 17:27:16 +0530

    fix: show absolute path

--- .config/nvim/lua/configs/telescope.lua
+++ .config/nvim/lua/configs/telescope.lua
@@ -1,17 +1,20 @@
 local options = {
-  defaults = {
-    selection_strategy = "reset",
-    layout_strategy = "horizontal",
-    layout_config = {
-      horizontal = {
-        prompt_position = "bottom",
-      },
-    },
-    sorting_strategy = "descending",
-    prompt_prefix = " ",
-    selection_caret = " ",
-    path_display = { "smart" },
-  },
+	defaults = {
+		selection_strategy = "reset",
+		layout_strategy = "horizontal",
+		layout_config = {
+			horizontal = {
+				prompt_position = "bottom",
+			},
+		},
+		sorting_strategy = "descending",
+		prompt_prefix = " ",
+		selection_caret = " ",
+		path_display = { "smart" },
+		file_ignore_patterns = {
+			"node_modules",
+		},
+	},
 }
 
 return options
